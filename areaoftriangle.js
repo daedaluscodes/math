501dc8cf6a16966a6ba75a04e36029fe53f7f32c
daedaluscodes <60836526+daedaluscodes@users.noreply.github.com>
2020-03-28 18:13:03 -0400

    Update areaoftriangle.js

--- areaoftriangle.js
+++ areaoftriangle.js
@@ -1,3 +1,4 @@
+//given 3 sides of a triangle, solve the area using the function with side a, b, and c.
 function area(a, b, c) {
 const perimeter = (a + b + c) / 2;
 let areaOfTriangle = Math.sqrt(perimeter * (perimeter - a) * (perimeter - b) * (perimeter - c));
